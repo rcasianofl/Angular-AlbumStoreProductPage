32bc1fc903c445ef7c50933127c881eb674c8807
rcasianofl <rcasiano@gmail.com>
2018-12-02 23:09:25 -0500

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  _productsUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
